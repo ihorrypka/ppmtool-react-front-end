6ea42fe9decdbe903331460dba4446127d6172ea
Ihor Rypka <ihor.rypka@gmail.com>
2020-02-18 19:50:21 +0200

    create ProjectItem and Navbar components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./App.css";
 import { Component } from "react";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dashboard />
-        <Dashboard />
-        <Dashboard />
+        <Header />
         <Dashboard />
       </div>
     );
