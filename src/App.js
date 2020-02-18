1500cdd86da43945adc36a9a44afe256198107ba
Ihor Rypka <ihor.rypka@gmail.com>
2020-02-18 20:27:32 +0200

    install bootstrap and import bootstrap.min.css in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Component } from "react";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
