fd3b9c91c3ff11f95f629a22f03d0351343a54d8
Ihor Rypka <ihor.rypka@gmail.com>
2020-03-11 11:31:05 +0200

    create Types and Reducers for Project Tasks

---
+++ src/reducers/backlogReducer.js
@@ -0,0 +1,38 @@
+import {
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK,
+  DELETE_PROJECT
+} from "../actions/types";
+import { bindActionCreators } from "redux";
+
+const initialState = {
+  project_tasks: [],
+  project_task: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_BACKLOG:
+      return {
+        ...state,
+        project_tasks: action.payload
+      };
+
+    case GET_PROJECT_TASK:
+      return {
+        ...state,
+        project_task: action.payload
+      };
+
+    case DELETE_PROJECT:
+      return {
+        ...state
+
+        // TO_DO
+      };
+
+    default:
+      return state;
+  }
+}
