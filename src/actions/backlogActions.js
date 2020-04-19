cd4078944177a6e519c47b6197e4f5e6b665c931
Ihor Rypka <ihor.rypka@gmail.com>
2020-04-19 21:36:56 +0300

    realize the functionality to delete Project Tasks

--- src/actions/backlogActions.js
+++ src/actions/backlogActions.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from "./types";
+import {
+  GET_ERRORS,
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK,
+} from "./types";
 
 export const addProjectTask = (backlog_id, project_task, history) => async (
   dispatch
@@ -68,6 +73,22 @@ export const updateProjectTask = (
     dispatch({
       type: GET_ERRORS,
       payload: err.response.data,
+    });
+  }
+};
+
+export const deleteProjectTask = (backlog_id, projectTask_id) => async (
+  dispatch
+) => {
+  if (
+    window.confirm(
+      `Are you sure you want to delete project task ${projectTask_id}? This action cannot be undone!`
+    )
+  ) {
+    await axios.delete(`/api/backlog/${backlog_id}/${projectTask_id}`);
+    dispatch({
+      type: DELETE_PROJECT_TASK,
+      payload: projectTask_id,
     });
   }
 };
